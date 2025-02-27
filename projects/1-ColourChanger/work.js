3027be145b3a23ef83e97f91348f970462042374
debarghya18 <141622842+debarghya18@users.noreply.github.com>
2025-02-27 18:58:16 +0000

    project 1 done

---
+++ projects/1-ColourChanger/work.js
@@ -0,0 +1,24 @@
+const buttons=document.querySelectorAll('.button')
+const body=document.querySelector('body')
+
+buttons.forEach((button)=>{
+    button.addEventListener('click', function(e){
+        switch (e.target.id){
+            case 'violet': body.style.backgroundColor=e.target.id;
+                           break;
+            case 'indigo': body.style.backgroundColor=e.target.id;
+                           break;
+            case 'blue': body.style.backgroundColor=e.target.id;
+                           break;
+            case 'green': body.style.backgroundColor=e.target.id;
+                           break;
+            case 'yellow': body.style.backgroundColor=e.target.id;
+                           break;
+            case 'orange': body.style.backgroundColor=e.target.id;
+                           break;
+            case 'red': body.style.backgroundColor=e.target.id;
+                           break;
+        }
+
+    })
+})
